bf8fa5402839f159ae21c56e78b65e1ba088006c
Sherlock-Yang03 <duongphucthang2003@gmail.com>
2023-12-04 21:39:33 +0700

    Commit to fetch

---
+++ client/src/pages/PrinterManagement.js
@@ -0,0 +1,3 @@
+export default function PrinterManagement() {
+    return (<h1>abc</h1>)
+}
--- client/src/routes/SPSORoutes.js
+++ client/src/routes/SPSORoutes.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import PrintHistory from "../pages/PrintHistory";
 import NotFound from "../pages/NotFound";
+import PrinterManagement from "../pages/PrinterManagement";
 
 // This is just a sample, can be changed later
 function SPSORoutes() {
   return (
     <Routes>
       <Route path='printer'> {/* Route quan ly may in */}
-        <Route index element={<h1>abc</h1>} /> {/* Route trang tong hop */}
+        <Route index element={<PrinterManagement />} /> {/* Route trang tong hop */}
         <Route path='add' element={<h1>ghi</h1>} />
       </Route>
       <Route path='printlog'> {/* Route xem lich su in cua nguoi dung */}
